f1aac4bc386070aa310a135f8a415ce9c0ecd487
Cameron McEfee <cm@cameronmcefee.com>
2023-09-01 22:01:13 -0700

    Initial Commit

---
+++ SwiftDataEnvironmentRelationshipsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataEnvironmentRelationshipsDemo
+//
+//  Created by Cameron McEfee on 9/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataEnvironmentRelationshipsDemo/SwiftDataEnvironmentRelationshipsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataEnvironmentRelationshipsDemoApp.swift
+//  SwiftDataEnvironmentRelationshipsDemo
+//
+//  Created by Cameron McEfee on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataEnvironmentRelationshipsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
